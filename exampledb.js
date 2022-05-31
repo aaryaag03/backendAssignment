81b5a45eaf4ed7ede709d08ad1b7c8504daed5d9
aaryaag03 <101268332+aaryaag03@users.noreply.github.com>
2022-05-31 15:50:17 +0530

    Delete exampledb.js

--- exampledb.js
+++
@@ -1,8 +0,0 @@
-const mysql=require('mysql');
-module.exports=mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "", //enter your mysql password here
-    database: "library",
-    port:3306,
-  });
